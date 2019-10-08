a539ac5916fcd54cb991172d1e48a544599ef326
romulo-soares <bromulosoares@gmail.com>
2019-10-08 09:17:51 -0300

    ignoring api auth and removing filter cors

--- src/main/java/br/edu/ifpb/tccii/materialzone/web/rest/BasicAuthController.java
+++ src/main/java/br/edu/ifpb/tccii/materialzone/web/rest/BasicAuthController.java
@@ -5,7 +5,9 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.annotations.ApiIgnore;
 
+@ApiIgnore
 @CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api/v1")
